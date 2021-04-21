72fd14358bc4e5272458dc1fb2ef97e5d6cb65b4
Anibal Mogollon <adjmogollon@gmail.com>
2021-04-21 12:46:58 -0400

    3.01 Eureka Server registrando microservicios

--- src/main/java/com/adjmogollon/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/adjmogollon/app/item/SpringbootServicioItemApplication.java
@@ -2,10 +2,14 @@ package com.adjmogollon.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="servicio-productos")
+
+@SuppressWarnings("deprecation")
+@EnableCircuitBreaker
+@EnableEurekaClient 
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
