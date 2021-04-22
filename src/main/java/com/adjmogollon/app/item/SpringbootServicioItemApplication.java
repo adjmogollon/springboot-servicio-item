caa4690b03612a80978b3a69f3e34df0c4dd200e
Anibal Mogollon <adjmogollon@gmail.com>
2021-04-22 16:16:28 -0400

    04 Spring Cloud Config Server centralizando la configuración

--- src/main/java/com/adjmogollon/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/adjmogollon/app/item/SpringbootServicioItemApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SuppressWarnings("deprecation")
 @EnableCircuitBreaker
 @EnableEurekaClient 
 @EnableFeignClients
--- src/main/java/com/adjmogollon/app/item/controllers/ItemController.java
+++ src/main/java/com/adjmogollon/app/item/controllers/ItemController.java
@@ -1,9 +1,18 @@
 package com.adjmogollon.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.security.auth.callback.TextOutputCallback;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +22,24 @@ import com.adjmogollon.app.item.models.Producto;
 import com.adjmogollon.app.item.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@RefreshScope
 @RestController
 public class ItemController {
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	//@Qualifier("serviceRestTemplate")
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+	
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(){
@@ -44,5 +64,26 @@ public class ItemController {
 		return item;
 		
 	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+	//public ResponseEntity<?> obtenerConfig(){
+		
+		log.info(texto);
+		Map<String,String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+				
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String,String>>(json,HttpStatus.OK);
+		
+	}
+	
+	
+	
 
 }
